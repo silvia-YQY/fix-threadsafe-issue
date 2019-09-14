0e66bd5ef8138d0a9e56396b9e11d77298379b1e
zhangyongwang <20225614@qq.com>
2019-09-14 11:43:39 +0800

    使用 ReentrantLock 解决多线程不安全问题

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,40 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
 
+    ReentrantLock lock = new ReentrantLock();
+
     public int getValue() {
-        return value;
+        lock.lock();
+        try {
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
-        value += i;
-        return value;
+    public int addAndGet(int i) {
+        lock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
-        value -= i;
-        return value;
+    public int minusAndGet(int i) {
+        lock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
