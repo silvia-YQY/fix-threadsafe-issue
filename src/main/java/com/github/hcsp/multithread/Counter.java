261f8066eade3f4e9c3e30472f54dbdd87e2a0f4
Hana <384073877@qq.com>
2019-09-24 21:43:00 +0800

    解决一个类的线程安全问题方法一

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,41 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    Lock calculator = new ReentrantLock();
 
     public int getValue() {
-        return value;
+        calculator.lock();
+        try {
+            return value;
+        } finally {
+            calculator.unlock();
+        }
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        calculator.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            calculator.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        calculator.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            calculator.unlock();
+
+        }
     }
 }
