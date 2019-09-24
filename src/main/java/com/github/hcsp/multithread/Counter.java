23983165b213f0c2b88a74c49842246402d667af
huangjiaxi <huangjiaxi@oak-inc.com>
2019-09-24 14:02:43 +0800

    修复 mvn test fail 问题

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -7,38 +7,38 @@ public class Counter {
     private int value = 0;
     private static Object lock = new Object();
     private static ReentrantLock reentrantLock = new ReentrantLock();
-    public static AtomicInteger count = new AtomicInteger(0);
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    // 非  AtomicInteger 方式使用
+    /*
     public int getValue() {
         return value;
     }
+    */
 
-    /**
-     *  方法一 ：给所在方法加上 synchronized 关键字
-     * @param i
-     * @return
-     */
-    // 加上一个整数i，并返回加之后的结果
+    public int getValue() {
+        return count.get();
+    }
+
+    //方法一 ：给所在方法加上 synchronized 关键字
     public synchronized int addAndGet1(int i) {
         value += i;
         return value;
     }
 
-    // 减去一个整数i，并返回减之后的结果
     public synchronized int minusAndGet1(int i) {
         value -= i;
         return value;
     }
 
-    /**
-     *  方法二：使用 Object 当作锁 synchronized(lock)
-     *  lock
-     *      可以是 Counter 内部定义的成员变量 lock
-     *      也可以直接用 this
-     * @param i
-     * @return
-     */
+    /*
+    * 方法二：使用 Object 当作锁 synchronized(lock)
+    * lock
+    * 可以是 Counter 内部定义的成员变量 lock
+    * 也可以直接用 this
+    */
     public int addAndGet2(int i) {
-        synchronized (lock){
+        synchronized (lock) {
             value += i;
             return value;
         }
@@ -46,44 +46,34 @@ public class Counter {
     }
 
     public int minusAndGet2(int i) {
-        synchronized (lock){
+        synchronized (lock) {
             value -= i;
             return value;
         }
     }
 
-    /**
-     *  方法三：使用 java.util.concurrent.locks.Lock 接口的实现类 ReentrantLock 可重入锁
-     * @param i
-     * @return
-     */
+    // 方法三：使用 java.util.concurrent.locks.Lock 接口的实现类 ReentrantLock 可重入锁
     public int addAndGet3(int i) {
         reentrantLock.lock();
-        try{
+        try {
             value += i;
             return value;
-        }finally {
+        } finally {
             reentrantLock.unlock();
         }
     }
 
     public int minusAndGet3(int i) {
         reentrantLock.lock();
-        try{
+        try {
             value -= i;
             return value;
-        }finally {
+        } finally {
             reentrantLock.unlock();
         }
     }
 
-    //
-
-    /**
-     *  方法四：使用 AtomicInteger
-     * @param i
-     * @return
-     */
+    // 方法四：使用 AtomicInteger
     public int addAndGet(int i) {
         return count.addAndGet(i);
     }
@@ -94,6 +84,8 @@ public class Counter {
 
     public static void main(String[] args) {
         Counter counter = new Counter();
+        AtomicInteger count = new AtomicInteger(0);
+        System.out.println(count.addAndGet(2));
         for (int i = 0; i < 1000; i++) {
 
             new Thread(() -> {
@@ -102,7 +94,7 @@ public class Counter {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                counter.addAndGet2(2);
+                counter.addAndGet(2);
                 System.out.println("add 2:" + counter.getValue());
             }).start();
 
@@ -112,7 +104,7 @@ public class Counter {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                counter.minusAndGet2(2);
+                counter.minusAndGet(2);
                 System.out.println("minus 2:" + counter.getValue());
             }).start();
         }
