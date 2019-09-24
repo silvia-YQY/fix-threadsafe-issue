045521db7690fbcba2dcb50bf454d230e34ab6c1
Hana <384073877@qq.com>
2019-09-24 22:00:57 +0800

    通过AtomicInteger 解决解决一个类的线程安全问题

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,41 +1,27 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Counter {
     private int value = 0;
-    Lock calculator = new ReentrantLock();
+    AtomicInteger a = new AtomicInteger(value);
+    AtomicInteger b = new AtomicInteger(value);
 
     public int getValue() {
-        calculator.lock();
-        try {
-            return value;
-        } finally {
-            calculator.unlock();
-        }
+        return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        calculator.lock();
-        try {
-            value += i;
-            return value;
-        } finally {
-            calculator.unlock();
-        }
+        a.addAndGet(i);
+        return value;
     }
+
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        calculator.lock();
-        try {
-            value -= i;
-            return value;
-        } finally {
-            calculator.unlock();
+        b.addAndGet(-i);
+        return value;
 
-        }
     }
 }
