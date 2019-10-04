4506adcb3eb0c60e0eb05169953610c178a9645a
胡凯波 <1069977596@qq.com>
2019-10-04 13:58:22 +0800

    用3中方法修复线程安全问题1

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -5,37 +5,39 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Counter {
     private int value = 0;
     private static Object lock = new Object();
-    private static AtomicInteger atomicInteger=new AtomicInteger();
+    private static AtomicInteger atomicInteger = new AtomicInteger();
 
-    public  synchronized int getValue() {
+    public synchronized int getValue() {
         return value;
-
     }
+
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int  addAndGet(int i) {
+    public synchronized int addAndGet(int i) {
         value += i;
         return value;
-
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public  synchronized int minusAndGet(int i) {
+    public synchronized int minusAndGet(int i) {
         value -= i;
         return value;
     }
+
     public int getValue1() {
         synchronized (lock) {
             return value;
         }
     }
-    public  int  addAndGet1(int i) {
+
+    public int addAndGet1(int i) {
         synchronized (lock) {
             value += i;
             System.out.println(value);
             return value;
         }
     }
-    public   int minusAndGet1(int i) {
+
+    public int minusAndGet1(int i) {
         synchronized (lock) {
             value -= i;
             System.out.println(value);
@@ -43,18 +45,19 @@ public class Counter {
 
         }
     }
-    public  int minusAndGet2(int i){
-        int b=atomicInteger.addAndGet(-i);
+
+    public int minusAndGet2(int i) {
+        int b = atomicInteger.addAndGet(-i);
         System.out.println(b);
         return b;
     }
 
 
-
     public static void main(String[] args) {
-        Counter counter=new Counter();
-        for (int i=0;i<100;i++){
-      //  new Thread(String.valueOf(counter.minusAndGet1(50))).start();
-        new Thread(String.valueOf(counter.minusAndGet2(222))).start();
-    } }
+        Counter counter = new Counter();
+        for (int i = 0; i < 100; i++) {
+            //  new Thread(String.valueOf(counter.minusAndGet1(50))).start();
+            new Thread(String.valueOf(counter.minusAndGet2(222))).start();
+        }
+    }
 }
