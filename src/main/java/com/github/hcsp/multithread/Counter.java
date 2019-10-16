86c866799d0b73fb2b005585fd3eee80bb7650e6
王杰 <46479343+wangjie-fourth@users.noreply.github.com>
2019-10-16 19:48:49 +0800

    线程安全 (#27)
    线程安全 (#27)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,23 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private int value = 0;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        value.addAndGet(i);
+        return value.intValue();
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        value.addAndGet(-i);
+        return value.intValue();
     }
 }
