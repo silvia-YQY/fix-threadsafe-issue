a6a57bf6f85950dd2b0dd2c5dfc1db276dd7f8e5
Zegendary <zxw1992513@qq.com>
2019-10-20 22:33:04 +0800

    三种方法解决线程安全问题

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,6 +1,51 @@
 package com.github.hcsp.multithread;
 
+//public class Counter {
+//    private int value = 0;
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public synchronized int addAndGet(int i) {
+//        value += i;
+//        return value;
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public synchronized int minusAndGet(int i) {
+//        value -= i;
+//        return value;
+//    }
+//}
+
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//public class Counter {
+//    private AtomicInteger count = new AtomicInteger(0);
+//
+//    public int getValue() {
+//        return count.get();
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public int addAndGet(int i) {
+//        return count.addAndGet(i);
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public int minusAndGet(int i) {
+//        return count.addAndGet(-i);
+//    }
+//}
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
+    Lock l = new ReentrantLock();
     private int value = 0;
 
     public int getValue() {
@@ -9,13 +54,17 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
+        l.lock();
         value += i;
+        l.unlock();
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
+        l.lock();
         value -= i;
+        l.unlock();
         return value;
     }
 }
