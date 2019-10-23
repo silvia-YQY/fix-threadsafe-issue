5b1807e32d5d7b3f0a5fbde543097dbbecebb262
orangec <604150318@qq.com>
2019-10-23 17:36:19 +0800

    java.util.concurrent.locks.Lock;

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,36 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.atomic.AtomicInteger;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
-    private static AtomicInteger atomicInteger = new AtomicInteger();
+    private static Lock lock=new ReentrantLock();
+    private static int value;
 
     public int getValue() {
-        return atomicInteger.get();
+        return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        return atomicInteger.addAndGet(i);
+        lock.lock();
+        try {
+            value +=i;
+            return value;
+        }finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        return atomicInteger.addAndGet(-i);
+        lock.lock();
+        try{
+            value -=i;
+            return value;
+        }finally {
+            lock.unlock();
+        }
     }
 }
