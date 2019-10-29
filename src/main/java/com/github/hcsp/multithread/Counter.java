8416c584dc98fbcd71e3801c5b9bcacf7971f139
stanley <154892616@qq.com>
2019-10-29 14:30:29 +0800

    fix threadsafe issue

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
@@ -9,13 +12,25 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
+        try {
+            lock.lock();
+            value += i;
+        } finally {
+            lock.unlock();
+        }
+
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
+        try {
+            lock.lock();
+            value -= i;
+        } finally {
+            lock.unlock();
+        }
+
         return value;
     }
 }
