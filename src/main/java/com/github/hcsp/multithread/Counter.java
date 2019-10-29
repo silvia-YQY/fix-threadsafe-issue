b1fb7232ef03c8b7139b8bede30efa0bfaea2277
shenwl <shenweilun96@gmail.com>
2019-10-29 19:13:13 +0800

    解决类的线程安全问题

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,6 +2,7 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private int value = 0;
+    private Object lock = new Object();
 
     public int getValue() {
         return value;
@@ -9,13 +10,17 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
+        synchronized (lock) {
+            value += i;
+        }
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
+        synchronized (lock) {
+            value -= i;
+        }
         return value;
     }
 }
