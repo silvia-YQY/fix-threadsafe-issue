65c52a61c856af823f8b27a6ca55d1c2fead2d4d
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-11-15 23:53:18 +0800

    fix: Use read-write lock to add thread-safety feature to Counter. (#43)
    fix: Use read-write lock to add thread-safety feature to Counter. (#43)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,42 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Counter {
     private int value = 0;
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
     public int getValue() {
-        return value;
+        readLock.lock();
+        try {
+            return value;
+        } finally {
+            readLock.unlock();
+        }
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        writeLock.lock();
+        try {
+            value += i;
+            return value;
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        writeLock.lock();
+        try {
+            value -= i;
+            return value;
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
