44ef613d6a7853a5fab13f5192a149981efe2cec
magicP <42081285+magicP@users.noreply.github.com>
2019-11-17 11:01:02 +0800

    修复线程安全问题 (#44)
    修复线程安全问题 (#44)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.multithread;
 
+
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     private int value = 0;
+    private ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
@@ -9,13 +13,23 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
-        return value;
+        try {
+            lock.lock();
+            value += 1;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
-        return value;
+        try {
+            lock.lock();
+            value -= 1;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
