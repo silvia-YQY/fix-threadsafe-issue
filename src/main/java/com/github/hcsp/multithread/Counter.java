fb3e280670a8b37646c77923a1cea419d8acbef8
emmmm <3108775859@qq.com>
2019-12-16 21:44:45 +0800

    BlockingQueue

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,35 +1,25 @@
 package com.github.hcsp.multithread;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Counter {
     private int value = 0;
-    final Lock lock = new ReentrantLock();
+    BlockingQueue<Integer> queue = new LinkedBlockingDeque<>();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        lock.lock();
-        try {
-            value += i;
-        }finally {
-            lock.unlock();
-        }
-        return value;
+    public int addAndGet(int i) throws InterruptedException {
+        queue.add(i + 1);
+        return queue.take();
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        lock.lock();
-        try {
-            value -= i;
-        }finally {
-            lock.unlock();
-        }
-        return value;
+    public int minusAndGet(int i) throws InterruptedException {
+        queue.add(i - 1);
+        return queue.take();
     }
 }
