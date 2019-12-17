10482747e3747e519866e7cea1b123e2ca32bc7d
emmmm <3108775859@qq.com>
2019-12-17 10:10:41 +0800

    AtomicInteger

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,22 +1,28 @@
 package com.github.hcsp.multithread;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntUnaryOperator;
 
 public class Counter {
-    private int value = 0;
-    AtomicInteger integer = new AtomicInteger();
+//    private int value = 0;
+    AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.get();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) throws InterruptedException {
-        return integer.addAndGet(i + 1);
+        return value.addAndGet(i);
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) throws InterruptedException {
-        return integer.addAndGet(i - 1);
+        return value.updateAndGet(new IntUnaryOperator() {
+            @Override
+            public int applyAsInt(int operand) {
+                return operand-i;
+            }
+        });
     }
 }
