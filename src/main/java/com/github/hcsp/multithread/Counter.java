f56f4594090eba83c2169c304bf513f1bca5079b
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-12-25 15:55:55 +0800

    解决一个类的线程安全问题 (#53)
    解决一个类的线程安全问题 (#53)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,23 @@
 package com.github.hcsp.multithread;
 
-public class Counter {
-    private int value = 0;
 
-    public int getValue() {
-        return value;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Counter {
+    private static AtomicInteger value = new AtomicInteger(0);
+
+    int getValue() {
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        value += i;
-        return value;
+    int addAndGet(int i) {
+
+        return value.addAndGet(i);
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        value -= i;
-        return value;
+    int minusAndGet(int i) {
+        return value.addAndGet(-i);
     }
 }
