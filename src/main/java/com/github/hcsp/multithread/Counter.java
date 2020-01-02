6e30ec7f2c8eb1b0db070fbfe1017bda60be4526
Maricaya <43136257+Maricaya@users.noreply.github.com>
2020-01-02 16:14:28 +0800

    lock 方法 (#57)
    lock 方法 (#57)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,33 @@
 package com.github.hcsp.multithread;
 
-public class Counter {
-    private int value = 0;
+import java.util.concurrent.locks.ReentrantLock;
 
-    public int getValue() {
+class Counter {
+    private int value = 0;
+    private ReentrantLock lock = new ReentrantLock();
+
+    int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        value += i;
-        return value;
+    void addAndGet(int i) {
+        lock.lock();
+        try {
+            value += i;
+        } finally {
+            lock.unlock();
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        value -= i;
-        return value;
+    void minusAndGet(int i) {
+        lock.lock();
+        try {
+            value -= i;
+        } finally {
+            lock.unlock();
+        }
     }
 }
+
