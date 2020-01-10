107d6bb2e0a5181a9bd8ce8736bde0a90e7cd1cf
NervousOrange <zch693922442@outlook.com>
2020-01-10 15:07:31 +0100

    from NervousOrange : Assignment 1 of MultiThread.

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Counter {
     private int value = 0;
-    private ReentrantLock lock = new ReentrantLock();
+    private final ReentrantLock lock = new ReentrantLock();
 
     public int getValue() {
         return value;
@@ -14,16 +14,22 @@ public class Counter {
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
         lock.lock();
-        value += i;
-        lock.unlock();
-        return value;
+        try {
+            value += i;
+            return value;
+        } finally {
+            lock.unlock();;
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
         lock.lock();
-        value -= i;
-        lock.unlock();
-        return value;
+        try {
+            value -= i;
+            return value;
+        } finally {
+            lock.unlock();
+        }
     }
 }
