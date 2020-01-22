0f66b46f63a11239dd2ea2809c27de2cd3709308
PingAn_NetBar <840495823@qq.com>
2020-01-22 23:36:11 +0800

    实例的synchronized方法把该实例当做锁。 (#64)
    实例的synchronized方法把该实例当做锁。 (#64)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -8,13 +8,13 @@ public class Counter {
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
+    public synchronized int addAndGet(int i) {
         value += i;
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
+    public synchronized int minusAndGet(int i) {
         value -= i;
         return value;
     }
