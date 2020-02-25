790c21ec4bab0d8e80d6c64db43d4528c07bdfef
silvia-YQY <545067444@qq.com>
2020-02-25 21:57:28 +0800

    三种方法实现

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,21 +1,69 @@
 package com.github.hcsp.multithread;
 
+// synchronized
+//public class Counter {
+//    private int value = 0;
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public synchronized int addAndGet(int i) {
+//        value += i;
+//        return value;
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public synchronized int minusAndGet(int i) {
+//        value -= i;
+//        return value;
+//    }
+//}
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+// AtomicInteger
 public class Counter {
-    private int value = 0;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public synchronized int addAndGet(int i) {
-        value += i;
-        return value;
+        return value.addAndGet(i);
     }
 
     // 减去一个整数i，并返回减之后的结果
     public synchronized int minusAndGet(int i) {
-        value -= i;
-        return value;
+        return value.addAndGet(-i);
     }
 }
+
+//import java.util.concurrent.locks.ReentrantLock;
+
+// ReentrantLock
+//public class Counter {
+//    private int value = 0;
+//    ReentrantLock lock = new ReentrantLock();
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    // 加上一个整数i，并返回加之后的结果
+//    public synchronized int addAndGet(int i) {
+//        value += i;
+//        lock.lock();
+//        return value;
+//    }
+//
+//    // 减去一个整数i，并返回减之后的结果
+//    public synchronized int minusAndGet(int i) {
+//        value -= i;
+//        lock.unlock();
+//        return value;
+//    }
+//}
